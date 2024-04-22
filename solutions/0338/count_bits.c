b4a18d81a3722879d71dd554ec7c63623c2abd90
Tiago Becerra Paolini <tpaolini@gmail.com>
2024-04-22 10:11:54 -0300

    Improved solution for Problem 388: Bit Counting Now the solution conforms to the requests of the problem (not using `__builtin_popcount()`, and performed in O(n) time complexity).

--- solutions/0338/count_bits.c
+++ solutions/0338/count_bits.c
@@ -1,13 +1,22 @@
-// Problem 338 - Counting Bits
-// https://leetcode.com/problems/counting-bits/
-
 #include <stdlib.h>
+#include <math.h>
+
+static int p2[17] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
+
+/*  Note: In consecutive binary integers, bit 0 flips each 2⁰ counts,
+    bit 1 flips each 2¹ counts, bit 2 flips each 2² counts, and so on...
+    The count keeps doubling each bit to the left.
+*/
 
 int* countBits(int n, int* returnSize){
-    int *ans = malloc((n+1) * sizeof(int));
+    int *ans = calloc(1, (n+1) * sizeof(int));
     *returnSize = n+1;
     for (int i = 0; i <= n; i++){
-        ans[i] = __builtin_popcount(i);
+        for (int j = 0; j < 17; j++){
+            if ((i / p2[j]) % 2 == 1){
+                ans[i] += 1;
+            }
+        }
     }
     return ans;
 }
